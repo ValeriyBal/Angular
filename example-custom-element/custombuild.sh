2ea2cdb4d0405d1c53fd7e3b36f64343ecc8391d
ValeriyBal <v4480844@gmail.com>
2020-06-02 14:58:17 +0300

    custombuild.sh example-custom-element es5

--- example-custom-element/custombuild.sh
+++ example-custom-element/custombuild.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ## ng build example-custom-element --prod --output-hashing=none && cat dist/example-custom-element/runtime.js dist/example-custom-element/polyfills.js dist/example-custom-element/scripts.js dist/example-custom-element/main.js > preview/angularapp.js
 
-cat dist/example-custom-element/runtime.js dist/example-custom-element/polyfills.js dist/example-custom-element/scripts.js dist/example-custom-element/main.js > preview/angularapp.js
+cat dist/example-custom-element/runtime-es5.js dist/example-custom-element/polyfills-es5.js dist/example-custom-element/scripts-es5.js dist/example-custom-element/main-es5.js > preview/angularapp.js
 
 
