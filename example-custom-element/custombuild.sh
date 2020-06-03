249f6167eb8e6868a0424add3c05031835f12e0d
ValeriyBal <v4480844@gmail.com>
2020-06-03 18:52:15 +0300

    path

--- example-custom-element/custombuild.sh
+++ example-custom-element/custombuild.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 npm run build:prod example-custom-element  && cat dist/example-custom-element/runtime.js dist/example-custom-element/main.js dist/example-custom-element/polyfills.js > preview/test.js
+
